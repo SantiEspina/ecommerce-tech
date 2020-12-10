6360442b06401580cc4cd2eda34d8b13fea96ff9
martinMamani <mpm.mamani@gmail.com>
2020-12-10 18:00:50 -0300

    insert.js

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,4 +1,4 @@
-/*const { DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -27,4 +27,3 @@ module.exports = (sequelize) => {
 }
 )
 };
-*/
