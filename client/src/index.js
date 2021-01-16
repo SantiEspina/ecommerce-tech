00e8453ef84e936ef7ef152dd9557db2c89746cc
Santi Espina <santi.espina14@gmail.com>
2021-01-16 14:13:02 -0300

    axios

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
+import axios from 'axios';
 import dotenv from "dotenv";
 dotenv.config();
 
